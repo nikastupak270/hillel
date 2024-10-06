342a4af3367bd607a50b20701dcfe0ed968ae3dd
Luda <luydmila.ra@gmail.com>
2024-10-06 11:56:12 +0300

    add koleos dicts

---
+++ koleos.py
@@ -0,0 +1,24 @@
+koleos_base = {
+    'title': 'Бензин 2,5 (171 к.с.) 4х2 АКП (CVT)',
+    'price': 1_263_400,
+    'has_rear_dynamic_led_lights': False,
+    'has_sunroof': dict(),
+    'has_rails_on_the_roof': True,
+    'has_cover_on_the_bumper': True,
+    'has_additional_tinting_of_rear_windows': True,
+    'has_18_inch_discs': True,
+    'has_metallic_paint': {'price': 19596},
+    'has_special_black_amethyst_paint': dict()
+}
+koleos_intense = {
+    'title': 'Бензин 2,5 (171 к.с.) 4х4 АКП (CVT)',
+    'price': 1_470_800,
+    'has_rear_dynamic_led_lights': True,
+    'has_sunroof': {'price': 29486},
+    'has_rails_on_the_roof': True,
+    'has_cover_on_the_bumper': True,
+    'has_additional_tinting_of_rear_windows': True,
+    'has_18_inch_discs': True,
+    'has_metallic_paint': {'price': 19596},
+    'has_special_black_amethyst_paint': {'price': 21758}
+}
