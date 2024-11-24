362fb3bec069ad2174e2b6b5699fab0fb51dda9d
Luda <luydmila.ra@gmail.com>
2024-11-24 21:31:18 +0200

    cars

--- car.py
+++ car.py
@@ -1,26 +1,50 @@
-class Car:
-    def __init__(self, year: int, make: str, model: str, fuel_cons: float):
-        self.year = year
-        self.make = make
+class Vehicle:
+    def __init__(self, brand: str, model: str ):
+        self.brand = brand
         self.model = model
-        self.fuel_cons = fuel_cons
-        self.mileage = 0
 
-    def drive(self):
-        print(f'Я авто марки {self.model}, їду по справам господаря')
-
-    @property
-    def cost_of_service(self):
-        return self.mileage * 7.6
+    def info(self):
+        print(f'Я авто марки {self.brand}, модель {self.model}')
 
 
-car_fiat = Car(2010,'Fiat','Tipo', fuel_cons=5.6)
-car_volvo = Car(2021,'Volvo','XC60', fuel_cons=9.1)
-car_nissan = Car(2021,'Nissan','Juke', fuel_cons=6.7)
-car_toyota = Car(2015,'Toyota','Yaris', fuel_cons=7.3)
-car_opel = Car(2019,'Opel', 'Astra', fuel_cons=8.1)
+class Car(Vehicle):
+    def __init__(self, brand: str, model: str, num_doors: int):
+        super().__init__(brand, model)
+        self.num_doors = num_doors
 
-car_fiat.mileage = 65000
-print(car_fiat.mileage)
-print(car_volvo.cost_of_service)
-car_toyota.drive()
+    def info(self):
+        print(f'Я авто марки {self.brand}, модель {self.model} і у мене є  {self.num_doors} дверей')
+
+
+class Bike(Vehicle):
+    def __init__(self, brand: str, model: str, type: str):
+        super().__init__(brand, model)
+        self.type = type
+
+    def info(self):
+        print(f'Я {self.type} велосипед марки {self.brand}, модель {self.model}')
+
+
+class Truck(Vehicle):
+    def __init__(self, brand: str, model: str, capacity: int):
+        super().__init__(brand, model)
+        self.capacity = capacity
+
+    def info(self):
+        print(f'Я вантажівка марки {self.brand}, модель {self.model} і я можу перевезти {self.capacity} тон')
+
+
+car_fiat = Car('Fiat','Tipo', num_doors=2)
+car_volvo = Car('Volvo','XC60', num_doors=5)
+car_nissan = Car('Nissan','Juke', num_doors=4)
+bike_formula = Bike('Formula','F-1', type='гірський')
+bike_xtreme = Bike('X-Treme','Rider', type='міський')
+truck_volvo = Truck('Volvo','FH16', capacity=16)
+truck_daf = Truck('DAF','CF', capacity=11)
+
+car_fiat.info()
+car_volvo.info()
+bike_formula.info()
+bike_xtreme.info()
+truck_volvo.info()
+truck_daf.info()
